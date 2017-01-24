d46ded6c5485eecf13e2a4680dbc07563cfbb1fa
dbielawski <damn@pcdamn.home>
2017-01-24 19:05:23 +0100

    Fonction de test pour fonctions forward et backward

--- fourier/fft.c
+++ fourier/fft.c
@@ -7,20 +7,52 @@
 fftw_complex
 *forward(int rows, int cols, unsigned short* g_img)
 {
-  (void)rows;
-  (void)cols;
-  (void)g_img;
-  return NULL;
-}
+  fftw_complex* in;
+  fftw_complex* out;
+  fftw_plan plan;
 
+  long int size = rows * cols;
+
+  in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
+  out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
+
+  for (long int i = 0; i < size; ++i)
+  {
+    in[i] = g_img[i];
+  }
+
+  plan = fftw_plan_dft_2d(rows , cols, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+  fftw_execute(plan);
+
+  fftw_destroy_plan(plan);
+  fftw_free(in);
+
+  return out;
+}
 
 unsigned short 
 *backward(int rows, int cols, fftw_complex* freq_repr)
 {
-  (void)rows;
-  (void)cols;
-  (void)freq_repr;
-  return NULL;
+  long int size = rows * cols;
+  unsigned short *img = malloc(size * sizeof(unsigned short));
+
+  fftw_plan plan;
+  fftw_complex* out;
+  out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
+
+  plan = fftw_plan_dft_2d(rows , cols, freq_repr, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+  fftw_execute(plan);
+
+  for (int i = 0; i < size; ++i)
+  {
+    double real = creal(out[i]);
+    img[i] = (unsigned short)(real / size);
+  }
+
+  fftw_destroy_plan(plan);
+  fftw_free(out);
+
+  return img;
 }
 
 void
--- fourier/test-fft.c
+++ fourier/test-fft.c
@@ -21,8 +21,36 @@ static void
 test_for_backward(pnm ims, char* name)
 {
   fprintf(stderr, "test_for_backward: ");
-  (void)ims;
-  (void)name;
+
+  long int cols = pnm_get_width(ims);
+  long int rows = pnm_get_height(ims);
+  pnm imd = pnm_new(cols, rows, PnmRawPpm);
+
+  unsigned short* gray = malloc(sizeof(unsigned short) * cols * rows);
+
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      gray[i * cols + j] = pnm_get_component(ims, i, j, 0);
+
+  fftw_complex* freq_repr = forward(rows, cols, gray);
+  unsigned short* fb_gray = backward(rows , cols, freq_repr);
+
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+    {
+      pnm_set_component(imd, i, j, 0, fb_gray[i * cols + j]);
+      pnm_set_component(imd, i, j, 1, fb_gray[i * cols + j]);
+      pnm_set_component(imd, i, j, 2, fb_gray[i * cols + j]);
+    }
+
+  char new_name[100];
+  strcat(new_name, "FB-");
+  strcat(new_name, name);
+  pnm_save(imd, PnmRawPpm, "new.ppm");
+  pnm_free(imd);
+  free(fb_gray);
+  free(gray);
+  fftw_free(freq_repr);
   fprintf(stderr, "OK\n");
 }
 
@@ -91,11 +119,12 @@ usage (char *s)
 
 int 
 main(int argc, char *argv[])
-{
+{  
   if (argc != 2)
     usage(argv[0]);
   pnm ims = pnm_load(argv[1]);
   run(ims, argv[1]);
+  pnm_free(ims);
 
   return EXIT_SUCCESS;
 }
