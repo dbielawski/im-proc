7a07b3eb822d8b47112a72c75f171723d20f243a
dbielawski <damn@pcdamn.home>
2017-01-29 20:12:15 +0100

    Fonctions freq2spectra et spectra2freq

--- fourier/fft.c
+++ fourier/fft.c
@@ -34,44 +34,47 @@ unsigned short
 *backward(int rows, int cols, fftw_complex* freq_repr)
 {
   long int size = rows * cols;
-  unsigned short *img = malloc(size * sizeof(unsigned short));
+  unsigned short *image = malloc(size * sizeof(unsigned short));
 
   fftw_plan plan;
   fftw_complex* out;
   out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
 
-  plan = fftw_plan_dft_2d(rows , cols, freq_repr, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+  plan = fftw_plan_dft_2d(rows , cols, freq_repr, out,
+    FFTW_BACKWARD,
+    FFTW_ESTIMATE);
+
   fftw_execute(plan);
 
   for (int i = 0; i < size; ++i)
   {
     double real = creal(out[i]);
-    img[i] = (unsigned short)(real / size);
+    image[i] = (unsigned short)(real / size);
   }
 
   fftw_destroy_plan(plan);
   fftw_free(out);
 
-  return img;
+  return image;
 }
 
 void
-freq2spectra(int rows, int cols, fftw_complex* freq_repr, float* as, float* ps) 
+freq2spectra(int rows, int cols, fftw_complex* freq_repr, float* as, float* ps)
 {
-  (void)rows;
-  (void)cols;
-  (void)freq_repr;
-  (void)as;
-  (void)ps;
-}
+  long int size = rows * cols;
 
+  for (int i = 0; i < size; ++i)
+  {
+    as[i] = cabs(freq_repr[i]);
+    ps[i] = carg(freq_repr[i]);
+  }
+}
 
 void 
 spectra2freq(int rows, int cols, float* as, float* ps, fftw_complex* freq_repr)
 {
-  (void)rows;
-  (void)cols;
-  (void)freq_repr;
-  (void)as;
-  (void)ps;
+  long int size = rows * cols;
+
+  for (int i = 0; i < size; ++i)
+    freq_repr[i] = as[i] * cos(ps[i]) + I * as[i] * sin(ps[i]);
 }
--- fourier/test-fft.c
+++ fourier/test-fft.c
@@ -12,6 +12,50 @@
 
 #include "fft.h"
 
+char* process_name(char* name)
+{
+  char *slash = strrchr(name, '/');
+  if (slash)
+  {
+    slash++;
+
+    int len = (strlen(slash));
+    char* res = malloc((strlen(slash) + 1) * sizeof(char));
+    strncpy(res, slash, len);
+    res[len] = '\0';
+    return res;
+  }
+  return name;
+}
+
+float find_max_amplitude(long int size, float* as)
+{
+  float max = 0.f;
+  for (int i = 0; i < size; ++i)
+  {
+    if (max < as[i])
+      max = as[i];
+  }
+  return max;
+}
+
+unsigned short clamp(int value, int min, int max)
+{
+  if (value < max && value > min)
+    return value;
+  else if (value < min)
+    return min;
+  else
+    return max;
+}
+
+void centrage(int rows, int cols, unsigned short* img)
+{
+  long int size = rows * cols;
+  for (int i = 0; i < size; ++i)
+    img[i] = img[i] * pow(-1, i);
+}
+
 /**
  * @brief test the forward and backward functions
  * @param pnm ims, the source image
@@ -24,7 +68,6 @@ test_for_backward(pnm ims, char* name)
 
   long int cols = pnm_get_width(ims);
   long int rows = pnm_get_height(ims);
-  pnm imd = pnm_new(cols, rows, PnmRawPpm);
 
   unsigned short* gray = malloc(sizeof(unsigned short) * cols * rows);
 
@@ -33,24 +76,24 @@ test_for_backward(pnm ims, char* name)
       gray[i * cols + j] = pnm_get_component(ims, i, j, 0);
 
   fftw_complex* freq_repr = forward(rows, cols, gray);
-  unsigned short* fb_gray = backward(rows , cols, freq_repr);
+  gray = backward(rows , cols, freq_repr);
 
+  pnm imd = pnm_new(cols, rows, PnmRawPpm);
   for (int i = 0; i < rows; ++i)
     for (int j = 0; j < cols; ++j)
     {
-      pnm_set_component(imd, i, j, 0, fb_gray[i * cols + j]);
-      pnm_set_component(imd, i, j, 1, fb_gray[i * cols + j]);
-      pnm_set_component(imd, i, j, 2, fb_gray[i * cols + j]);
+      pnm_set_component(imd, i, j, 0, gray[i * cols + j]);
+      pnm_set_component(imd, i, j, 1, gray[i * cols + j]);
+      pnm_set_component(imd, i, j, 2, gray[i * cols + j]);
     }
 
-  char new_name[100];
-  strcat(new_name, "FB-");
-  strcat(new_name, name);
-  pnm_save(imd, PnmRawPpm, "new.ppm");
+  char new_name[100] = "FB-";
+  strcat(new_name, process_name(name));
+  pnm_save(imd, PnmRawPpm, new_name);
   pnm_free(imd);
-  free(fb_gray);
   free(gray);
   fftw_free(freq_repr);
+
   fprintf(stderr, "OK\n");
 }
 
@@ -66,6 +109,41 @@ test_reconstruction(pnm ims, char* name)
   fprintf(stderr, "test_reconstruction: ");
   (void)ims;
   (void)name;
+
+  long int cols = pnm_get_width(ims);
+  long int rows = pnm_get_height(ims);
+
+  float *as = (float*)malloc(rows * cols * sizeof(float));
+  float *ps = (float*)malloc(rows * cols * sizeof(float));
+
+  unsigned short *gray = malloc(rows * cols * sizeof(unsigned short));
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      gray[i * cols + j] = pnm_get_component(ims, i, j, 0);
+
+  fftw_complex *freq_repr = forward(rows, cols, gray);
+  freq2spectra(rows, cols, freq_repr, as, ps);
+  spectra2freq(rows, cols, as, ps, freq_repr);
+  unsigned short* m_gray = backward(rows , cols, freq_repr);
+
+  pnm imd = pnm_new(cols, rows, PnmRawPpm);
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+    {
+      pnm_set_component(imd, i, j, 0, m_gray[i * cols + j]);
+      pnm_set_component(imd, i, j, 1, m_gray[i * cols + j]);
+      pnm_set_component(imd, i, j, 2, m_gray[i * cols + j]);
+    }
+
+  free(as);
+  free(ps);
+  fftw_free(freq_repr);
+  free(gray);
+  char new_name[100] = "FB-ASPS-";
+  strcat(new_name, process_name(name));
+  pnm_save(imd, PnmRawPpm, new_name);
+  pnm_free(imd);
+
   fprintf(stderr, "OK\n");
 }
 
@@ -81,6 +159,60 @@ test_display(pnm ims, char* name)
   fprintf(stderr, "test_display: ");
   (void)ims;
   (void)name;
+
+  long int cols = pnm_get_width(ims);
+  long int rows = pnm_get_height(ims);
+
+  float *as = (float*)malloc(rows * cols * sizeof(float));
+  float *ps = (float*)malloc(rows * cols * sizeof(float));
+
+  unsigned short *gray = malloc(rows * cols * sizeof(unsigned short));
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      gray[i * cols + j] = pnm_get_component(ims, i, j, 0);
+
+  centrage(rows, cols, gray);
+  fftw_complex *freq_repr = forward(rows, cols, gray);
+  freq2spectra(rows, cols, freq_repr, as, ps);
+
+  pnm imd_as = pnm_new(cols, rows, PnmRawPpm);
+  pnm imd_ps = pnm_new(cols, rows, PnmRawPpm);  
+
+  float k = 0.1f;
+  float as_max = find_max_amplitude(rows * cols, as);
+
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+    {
+      pnm_set_component(imd_as, i, j, 0,
+        clamp(pow(as[i * cols + j] / as_max, k) * 255.f, 0, pnm_maxval));
+      pnm_set_component(imd_as, i, j, 1,
+        clamp(pow(as[i * cols + j] / as_max, k) * 255.f, 1, pnm_maxval));
+      pnm_set_component(imd_as, i, j, 2,
+        clamp(pow(as[i * cols + j] / as_max, k) * 255.f, 2, pnm_maxval));
+
+      pnm_set_component(imd_ps, i, j, 0, ps[i * cols + j]);
+      pnm_set_component(imd_ps, i, j, 1, ps[i * cols + j]);
+      pnm_set_component(imd_ps, i, j, 2, ps[i * cols + j]);
+    }
+
+
+  free(as);
+  free(ps);
+  fftw_free(freq_repr);
+  free(gray);
+
+  char new_as[50] = "AS-";
+  strcat(new_as, process_name(name));
+  pnm_save(imd_as, PnmRawPpm, new_as);
+    
+  char new_ps[50] = "PS-";
+  strcat(new_ps, process_name(name));
+  pnm_save(imd_ps, PnmRawPpm, new_ps);
+
+  pnm_free(imd_as);
+  pnm_free(imd_ps);
+
   fprintf(stderr, "OK\n");
 }
 
