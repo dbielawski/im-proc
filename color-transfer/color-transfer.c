b14c30975e84af1e88140ff697ad31aa7710b507
Damien Bielawski <dbielawski@emi.u-bordeaux1.fr>
2017-03-06 18:15:05 +0100

    marche pas mais ...

--- color-transfer/color-transfer.c
+++ color-transfer/color-transfer.c
@@ -41,7 +41,7 @@ static float LMS2RGB[D][D] = {
 };
 
 
-int clamp(int min, int max, int value)
+static int clamp(int min, int max, int value)
 {
   int x = value;
   if (value < min)
@@ -51,7 +51,7 @@ int clamp(int min, int max, int value)
   return x;
 }
 
-float* rgb2lms(unsigned short* RGB, int cols, int rows)
+static float* rgb2lms(unsigned short* RGB, int cols, int rows)
 {
   float* LMS = malloc(sizeof(float) * cols * rows * 3);
 
@@ -75,7 +75,7 @@ float* rgb2lms(unsigned short* RGB, int cols, int rows)
   return LMS;
 }
 
-float* lms2lab(float* LMS, int cols, int rows)
+static float* lms2lab(float* LMS, int cols, int rows)
 {
   float* LAB = malloc(sizeof(float) * cols * rows * 3);
 
@@ -88,14 +88,15 @@ float* lms2lab(float* LMS, int cols, int rows)
         for (int l = 0; l < 3; ++l)
           tmp += LMS2LAB[k][l] * LMS[i * 3 * cols + (3 * j) + l];
 
-        LAB[i * 3 * cols + (3 * j) + k] =
-          (unsigned short)clamp(0, 255, (unsigned short)tmp);
+        LAB[i * 3 * cols + (3 * j) + k] = tmp;
+        // LAB[i * 3 * cols + (3 * j) + k] =
+        //   (unsigned short)clamp(0, 255, (unsigned short)tmp);
       }
 
   return LAB;
 }
 
-float* lab2lms(float* LAB, int cols, int rows)
+static float* lab2lms(float* LAB, int cols, int rows)
 {
   float* LMS = malloc(sizeof(float) * cols * rows * 3);
 
@@ -110,14 +111,16 @@ float* lab2lms(float* LAB, int cols, int rows)
 
         tmp = pow(10, tmp);
 
-        LMS[i * 3 * cols + (3 * j) + k] =
-          (unsigned short)clamp(0, 255, (unsigned short)tmp);
+        LMS[i * 3 * cols + (3 * j) + k] = tmp;
+
+        // LMS[i * 3 * cols + (3 * j) + k] =
+        //   (unsigned short)clamp(0, 255, (unsigned short)tmp);
       }
 
   return LMS;
 }
 
-unsigned short* lms2rgb(float* LMS, int cols, int rows)
+static unsigned short* lms2rgb(float* LMS, int cols, int rows)
 {
   unsigned short* RGB = malloc(sizeof(unsigned short) * cols * rows * 3);
 
@@ -137,7 +140,7 @@ unsigned short* lms2rgb(float* LMS, int cols, int rows)
   return RGB;
 }
 
-float* means(float* buffer, int cols, int rows)
+static float* means(float* lab, int cols, int rows)
 {
   float* means = malloc(sizeof(float) * 3);
 
@@ -147,38 +150,107 @@ float* means(float* buffer, int cols, int rows)
   for (int i = 0; i < rows; ++i)
     for (int j = 0; j < cols; ++j)
       for (int k = 0; k < 3; ++k)
-        means[k] += buffer[i * 3 * cols + (3 * j) + k];
+        means[k] += lab[i * 3 * cols + (3 * j) + k];
 
   for (int i = 0; i < 3; ++i)
     means[i] /= cols * rows;
+
+  return means;
 }
 
-static void process(char *ims, char *imt, char* imd)
+static float* standardDeviations(float* means, float* lab, int cols, int rows)
 {
-  fprintf(stderr, "color-transfert: ");
+  float* deviations = malloc(sizeof(float) * 3);
 
-  pnm im_out = pnm_load(ims);
-  int cols = pnm_get_width(im_out);
-  int rows = pnm_get_height(im_out);
+  for (int i = 0; i < 3; ++i)
+    deviations[i] = 0.f;
 
-  unsigned short* buffer = pnm_get_image(im_out);
-  float* lms = rgb2lms(buffer, cols, rows);
-  float* lab = lms2lab(lms, cols, rows);
-  lms = lab2lms(lab, cols, rows);
-  unsigned short* rgb = lms2rgb(lms, cols, rows);
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      for (int k = 0; k < 3; k++)
+      {
+        deviations[k] += lab[i * cols * 3 + j * 3 + k] - means[k] *
+                        lab[i * cols * 3 + j * 3 + k] - means[k];
+      }
+
+  for (int i = 0; i < 3; ++i)
+    deviations[i] = sqrt(deviations[i]);
+
+  return deviations;
+}
+
+static float* transfer(float* deviation_s, float* deviation_t, float* lab_t, float* means_s, float* means_t, int cols, int rows)
+{
+  float* lab_d = malloc(sizeof(unsigned short) * cols * rows * 3);
 
   for (int i = 0; i < rows; ++i)
     for (int j = 0; j < cols; ++j)
       for (int k = 0; k < 3; ++k)
-        buffer[i * 3 * cols + (3 * j) + k] =
-          rgb[i * 3 * cols + (3 * j) + k];
+      {
+        lab_d[i * cols * 3 + j * 3 + k] = lab_t[i * cols * 3 + j * 3 + k] - means_t[k];
+        lab_d[i * cols * 3 + j * 3 + k] *= deviation_s[k] / deviation_t[k];
 
-  pnm_save(im_out, PnmRawPpm, imd);
+        lab_d[i * cols * 3 + j * 3 + k] += means_s[k];
+      }
 
-  // free(lab);
-  free(lms);
+  return lab_d;
+}
+
+static void fillBuffer(unsigned short* toFill, unsigned short* in, int cols , int rows)
+{
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      for (int k = 0; k < 3; ++k)
+        toFill[i * 3 * cols + (3 * j) + k] = in[i * 3 * cols + (3 * j) + k];
+}
+
+static void process(char *ims, char *imt, char* imd)
+{
+  fprintf(stderr, "color-transfer: ");
+
+  pnm im_s = pnm_load(ims);
+  pnm im_t = pnm_load(imt);
+
+  int cols = pnm_get_width(im_s);
+  int rows = pnm_get_height(im_t);
+
+  unsigned short* img_s = pnm_get_image(im_s);
+  unsigned short* img_t = pnm_get_image(im_t);
+
+  float* lms_s = rgb2lms(img_s, cols, rows);
+  float* lms_t = rgb2lms(img_t, cols, rows);
+
+  float* lab_s = lms2lab(lms_s, cols, rows);
+  float* lab_t = lms2lab(lms_t, cols, rows);
+
+  float* means_s = means(lab_s, cols, rows);
+  float* means_t = means(lab_t, cols, rows);
+
+  // Deviation
+  float* deviation_s = standardDeviations(means_s, lab_s, cols, rows);
+  float* deviation_t = standardDeviations(means_t, lab_t, cols, rows);
+
+  // Transfer
+  float* lab_d = transfer(deviation_s, deviation_t, lab_t, means_s, means_t, cols, rows);
+
+  float* lms_d = lab2lms(lab_d, cols, rows);
+  unsigned short* rgb = lms2rgb(lms_d, cols, rows);
+
+  pnm im_d = pnm_new(cols, rows, PnmRawPpm);
+  unsigned short* buffer = pnm_get_image(im_d);
+  fillBuffer(buffer, rgb, cols, rows);
+
+  pnm_save(im_d, PnmRawPpm, imd);
+
+  free(lab_s);
+  free(lab_d);
+  free(lms_s);
+  free(lms_t);
+  free(lms_d);
   free(rgb);
-  pnm_free(im_out);
+  free(buffer);
+  pnm_free(im_s);
+  pnm_free(im_t);
 
   fprintf(stderr, "OK\n");
 }
